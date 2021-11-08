bbe5897cad6794003676294e9a143b177a602585
Leytek <leytek@yandex.ru>
2021-11-08 23:37:20 +0300

    feat: implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
 
+async function copyDir(){
+  const baseDirPath = new URL('./files', import.meta.url).pathname.slice(1),
+    copyDirPath = new URL('./files-copy', import.meta.url).pathname.slice(1),
+    baseDir = await fs.readdir(baseDirPath),
+    copyDir = await fs.readdir(copyDirPath);
+
+  await fs.mkdir(copyDirPath, {recursive: true});
+  for(let file of copyDir){
+    if(!baseDir.includes(file))
+      fs.rm(path.join(copyDirPath, file));
+  }
+  for(let file of baseDir){
+    fs.copyFile(path.join(baseDirPath, file), path.join(copyDirPath, file));
+  }
+}
+
+copyDir();
