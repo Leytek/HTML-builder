6e1a3ddebe3c1b3060612acbb392112d7a74688c
Leytek <leytek@yandex.ru>
2022-05-24 20:42:51 +0300

    refactor: move 03-files-infolder to promises

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,17 +1,15 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 import url from 'url';
 
 const dirPath = url.fileURLToPath(new URL('./secret-folder', import.meta.url));
+const files = await fs.readdir(dirPath, {withFileTypes: true});
 
-fs.readdir(dirPath, {withFileTypes: true}, (err, files) => {
-  for (let file of files){
-    fs.stat(path.join(dirPath, file.name), (err, stats) => {
-      if(stats.isFile()){
-        const ext = path.extname(file.name);
-        const base = path.basename(file.name, ext);
-        console.log(`${base} - ${ext} - ${(stats.size / 1024).toFixed(3)}kb`);
-      }
-    });
+files.forEach(async file => {
+  const stat = await fs.stat(path.join(dirPath, file.name));
+  if(stat.isFile()){
+    const ext = path.extname(file.name);
+    const base = path.basename(file.name, ext);
+    console.log(`${base} - ${ext} - ${(stat.size / 1024).toFixed(3)}kb`);
   }
 });
